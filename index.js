040118f83ba8f0e45e57e45a9ff876c5405ae474
yangcurve <utsu0621@gmail.com>
2024-11-09 02:45:30 +0900

    .

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line
 import templatePackageJson from './template/package.json' with { type: 'json' }
 import { cpSync } from 'node:fs'
 
@@ -8,9 +7,10 @@ if (!projectNameInput) {
   console.error('project name is required')
   process.exit(1)
 }
+
 const useCurrentDirectory = projectNameInput === '.'
 
-const projectName = useCurrentDirectory ? process.cwd().split('/').at(-1)! : projectNameInput
+const projectName = useCurrentDirectory ? process.cwd().split('/').at(-1) : projectNameInput
 const projectPath = `${process.cwd()}/${useCurrentDirectory ? '' : projectName}`
 
 const newPackageJson = structuredClone(templatePackageJson)
